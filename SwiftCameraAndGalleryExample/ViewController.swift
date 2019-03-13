0be12b61fb738108ce0f0e024bbc90c04452b2f8
MacStudent <macstudent@126-imac22.local>
2019-03-13 19:11:16 -0400

    designed and added function prototypes

--- SwiftCameraAndGalleryExample/ViewController.swift
+++ SwiftCameraAndGalleryExample/ViewController.swift
@@ -10,12 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgPic: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         //democommit code
     }
 
-
+    @IBAction func btnCamClick(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func btnGalleryClick(_ sender: UIBarButtonItem) {
+    }
 }
 
